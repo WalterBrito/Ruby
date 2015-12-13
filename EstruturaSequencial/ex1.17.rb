c7ce12f9972d0556ac692bed86cf87001ed38790
WalterBrito <waltter.britto@gmail.com>
2015-12-13 13:22:19 -0200

    Ruby exercicios e exemplos.

--- EstruturaSequencial/ex1.17.rb
+++ EstruturaSequencial/ex1.17.rb
@@ -14,7 +14,7 @@ c. misturar latas e galões, de forma que o preço seja o menor.
 
 puts "=========================================================="
 
-print 'Digite o tamanho da area (Metros): ' # Pedir a area ao utilizador
+print "Digite o tamanho da area (Metros): " # Pedir a area ao utilizador
 area = gets.chomp.to_i
 
 litros = 18.0 
@@ -26,11 +26,11 @@ areaLitroTinta = 6.0
 tintaNecessaria = area / areaLitroTinta # Litros de tinta necessarios para a pintura
 
 if tintaNecessaria == 1
-    puts 'Vai precisar de um litro de tinta.'.to_s
+    puts "Vai precisar de um litro de tinta.".to_s
 elsif tintaNecessaria != 1 and tintaNecessaria > 0
-    puts 'Vai precisar de #{tintaNecessaria} litros de tinta'.to_s
+    puts "Vai precisar de #{tintaNecessaria} litros de tinta".to_s
 else
-    puts 'Invalido! tente outra vez.'.to_s
+    puts "Invalido! tente outra vez.".to_s
 end
    
 
@@ -41,20 +41,20 @@ restanteTinta = (tintaNecessaria % litros) # Devolve a qtd de tinta que fica em 
 # Com base nesse excesso, vemos se o que e usado da ultima lata justifica a sua compra
 # Temos o valor excessivo a partir de (3.6*4) porque e o valor em que as latas pequenas ficam mais caras que uma grande
 if (litros - restanteTinta) < (3.6*4)
-    galoesNecessario = (litros - restanteTinta) / galoes
+    galoesNecessario = ((litros - restanteTinta) / galoes)
 end
 
-# Devolve o preco da mistura
+# Devolve o preco da mistura (litros e galoes)
 
 if galoesNecessario != 0
-    precoFinal = (precoLitro * ((litrosNecessario)-1).to_i + (galoesNecessario).to_i * precoGaloes)
+    precoFinal = ((precoLitro * ((litrosNecessario)-1).to_i + (galoesNecessario).to_i * precoGaloes)).round(2)
 else
-    precoFinal = precoLitro * (litrosNecessario).to_i
+    precoFinal = (precoLitro * (litrosNecessario).to_i).round(2)
 end
 
-puts 'Preco final: #{precoFinal}'.to_s
-puts 'Preco em litros: #{litrosNecessario * precoLitro}'.to_s
-puts 'Preco em galoes: #{precoGaloes} #{tintaNecessaria} #{galoes}'.to_s
+puts "Preco final: #{precoFinal}".to_s
+puts "Preco em litros: #{litrosNecessario * precoLitro}".to_s
+puts "Preco em galoes: #{precoGaloes * (tintaNecessaria / galoes)}".to_s
 
 
 puts "=========================================================="
