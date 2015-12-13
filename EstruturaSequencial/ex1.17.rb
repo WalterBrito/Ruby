6ddefc718b9d1aae89d4dfeed66429b7d8b36494
WalterBrito <waltter.britto@gmail.com>
2015-12-13 13:07:50 -0200

    Ruby exercicios e exemplos.

--- EstruturaSequencial/ex1.17.rb
+++ EstruturaSequencial/ex1.17.rb
@@ -14,25 +14,47 @@ c. misturar latas e galões, de forma que o preço seja o menor.
 
 puts "=========================================================="
 
-print "Digite o tamanho da área a ser pintada (Metros): "
-areaMetros = gets.chomp.to_i
+print 'Digite o tamanho da area (Metros): ' # Pedir a area ao utilizador
+area = gets.chomp.to_i
 
-areaLitros = areaMetros / 6
+litros = 18.0 
+galoes = 3.6 
+precoLitro = 80.0 
+precoGaloes = 25.0 
+areaLitroTinta = 6.0 
 
-capacidadeLata = 18 # 1 lata suporta 18 litros de tinta
-precoLata = 80.00
-capacidadeGaloes = 3.6
-precoGalao = 25.00
+tintaNecessaria = area / areaLitroTinta # Litros de tinta necessarios para a pintura
 
-tintaLatas = (areaLitros / capacidadeLata).round(2)
-tintaGaloes =  (areaLitros / capacidadeGaloes).round(2)
-precoTotalLata =  (tintaLatas * precoLata).round(2)
-precoTotalGaloes = (tintaGaloes * precoGalao).round(2)
- 
+if tintaNecessaria == 1
+    puts 'Vai precisar de um litro de tinta.'.to_s
+elsif tintaNecessaria != 1 and tintaNecessaria > 0
+    puts 'Vai precisar de #{tintaNecessaria} litros de tinta'.to_s
+else
+    puts 'Invalido! tente outra vez.'.to_s
+end
+   
 
-puts "Voce comprou #{precoTotalLata} latas com 18 litros.".to_s
-puts "Voce comprou #{precoTotalGaloes} latas com 3.6 litros.".to_s
-puts "Voce comprou #{precoTotalLata} latas e #{precoTotalGaloes} galoes.".to_s
+litrosNecessario = tintaNecessaria / litros # Devolve a qtdade de litros necessarias para pintar a area prentendida
+
+restanteTinta = (tintaNecessaria % litros) # Devolve a qtd de tinta que fica em excesso dos litros.
+
+# Com base nesse excesso, vemos se o que e usado da ultima lata justifica a sua compra
+# Temos o valor excessivo a partir de (3.6*4) porque e o valor em que as latas pequenas ficam mais caras que uma grande
+if (litros - restanteTinta) < (3.6*4)
+    galoesNecessario = (litros - restanteTinta) / galoes
+end
+
+# Devolve o preco da mistura
+
+if galoesNecessario != 0
+    precoFinal = (precoLitro * ((litrosNecessario)-1).to_i + (galoesNecessario).to_i * precoGaloes)
+else
+    precoFinal = precoLitro * (litrosNecessario).to_i
+end
+
+puts 'Preco final: #{precoFinal}'.to_s
+puts 'Preco em litros: #{litrosNecessario * precoLitro}'.to_s
+puts 'Preco em galoes: #{precoGaloes} #{tintaNecessaria} #{galoes}'.to_s
 
 
 puts "=========================================================="
